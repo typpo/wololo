9bac9ea690c71504ca43ea0d55fd9cc2d48ab7fa
Ian <ianw_github@ianww.com>
2014-12-22 18:15:00 +0000

    sweep work

--- sweep.js
+++ sweep.js
@@ -7,14 +7,12 @@ exports = module.exports = function() {
 
 function doSweep() {
   // Try to clean up. But this is not guaranteed to happen.
-  // TODO if timeout works, can just decr countkey. This is the cleanup
-  // function that works whenever.
   var timerKey = me.getTimerKey_(key, cat_key);
   var now = +new Date();
 
   client.zrangebyscore([timerKey, 0, now], function(err, results) {
     if (err) {
-      // TODO handle this?
+      // TODO handle or record this
       return;
     }
     results.forEach(function(countKey) {
