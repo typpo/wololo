fb80db47a635d6bbf5c4236ba9f714364cd3b62b
Ian <typppo@gmail.com>
2015-01-08 00:14:22 -0800

    standalone get

--- public/js/wololo_external.js
+++ public/js/wololo_external.js
@@ -1,6 +1,8 @@
 // This file served on external partner sites.
 
 !(function() {
+  'use strict';
+  var HOST = 'http://wololo.com';
   if (window.wololo) {
     throw 'Could not load wololo - do you already have it on the page?';
     return;
@@ -8,10 +10,33 @@
 
   window.wololo = {
     record: function(name, key, opts) {
-      name = name || 'view';
+      //var name = opts['name'] || 'view';
+      var cat_key = opts['cat_key'];
 
-      // ...
+      var url = HOST + '/viewing?key=' + key;
+      if (cat_key) {
+        url += '&cat_key=' + cat_key;
+      }
+
+      get(url, function(xmldoc) {
+        // ...
+      });
     },
   };
+
+  function get(url, callback) {
+    if (!window.XMLHttpRequest) {
+      return;
+    }
+
+    var xmlDoc = new XMLHttpRequest();
+    xmlDoc.open('GET', url, true);
+    xmlDoc.onreadystatechange = function() {
+      if (xmlDoc.readyState === 4 && xmlDoc.status === 200) {
+        callback(xmlDoc);
+      }
+    }
+    xmlDoc.send();
+  }
 })();
 
