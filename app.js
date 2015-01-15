51c57f6d552fbb0fd19150b1827750fe93662e24
Ian <typppo@gmail.com>
2015-01-14 20:13:06 -0800

    fix app.js

--- app.js
+++ app.js
@@ -1,3 +1,82 @@
-// Entry point for the app.
+// Start wololo sweep.
+console.log('Starting sweep...');
 require('./sweep').startSweep();
-require('keystone');
+
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').load();
+
+// Require keystone
+var keystone = require('keystone'),
+	swig = require('swig');
+
+// Disable swig's bulit-in template caching, express handles it
+swig.setDefaults({ cache: false });
+
+keystone.init({
+
+	'name': 'wololo',
+	'brand': 'wololo',
+
+	'less': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'swig',
+
+	'custom engine': swig.renderFile,
+
+	'emails': 'templates/emails',
+
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+	'cookie secret': 'n88z?G-,Vt(,l^H<>akQg$1vnh8Z_Br82kg(EIP{?M(o9pa2!Grrg}gGz6p@e]~p',
+
+  'port': 14000,
+});
+
+keystone.import('models');
+
+keystone.set('locals', {
+	_: require('underscore'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable
+});
+
+keystone.set('routes', require('./routes'));
+
+keystone.set('email locals', {
+	logo_src: '/images/logo-email.gif',
+	logo_width: 194,
+	logo_height: 76,
+	theme: {
+		email_bg: '#f9f9f9',
+		link_color: '#2697de',
+		buttons: {
+			color: '#fff',
+			background_color: '#2697de',
+			border_color: '#1a7cb7'
+		}
+	}
+});
+
+keystone.set('email rules', [{
+	find: '/images/',
+	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/images/' : 'http://localhost:3000/images/'
+}, {
+	find: '/keystone/',
+	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/keystone/' : 'http://localhost:3000/keystone/'
+}]);
+
+keystone.set('email tests', require('./routes/emails'));
+
+keystone.set('nav', {
+	'enquiries': 'enquiries',
+	'users': 'users'
+});
+
+console.log('Starting keystone...');
+keystone.start();
