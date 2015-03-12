4bd965e07f2ffe0d9fb62d4accb61f3744ffe731
Ian <typppo@gmail.com>
2015-03-11 21:49:04 -0700

    minify js and add notification library

--- routes/views/wololo.js
+++ routes/views/wololo.js
@@ -1,4 +1,12 @@
+var uglify = require('uglify-js');
+var code = null;
+
 exports = module.exports = function(req, res) {
-  // TODO minify this.
-  res.sendfile('public/js/wololo_external.js');
+  if (!code) {
+    console.log('Minifying public js for the first time...');
+    var files = ['public/js/lib/humane.js', 'public/js/wololo_external.js'];
+    var result = uglify.minify(files);
+    code = result.code;
+  }
+  res.send(code);
 };
